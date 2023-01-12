6ef8501c01129920c3d32cf035925258f65eeb11
Ming Li <Mingrkli@gmail.com>
2023-01-12 15:50:05 -0800

    testing

--- js/videoCard.js
+++ js/videoCard.js
@@ -10,34 +10,34 @@ fetch(videoSource).then((response) => {
             videoAlt[i].classList.remove('hidden');
         }
     }
-    else {
-        const cardVideos = document.querySelectorAll('.cards');
+    // else {
+    //     const cardVideos = document.querySelectorAll('.cards');
 
-        for (let i = 0; i < cardVideos.length; i++) {
-            let currentCard = cardVideos[i];
-            let currentVideo = currentCard.querySelector('.card-back-video')
+    //     for (let i = 0; i < cardVideos.length; i++) {
+    //         let currentCard = cardVideos[i];
+    //         let currentVideo = currentCard.querySelector('.card-back-video')
         
-            currentCard.addEventListener('mouseover', (e) => {
-                currentVideo.play();
-            })
+    //         currentCard.addEventListener('mouseover', (e) => {
+    //             currentVideo.play();
+    //         })
             
-            currentCard.addEventListener('mouseleave', (e) => {
-                currentVideo.pause();
-            })
-        }
+    //         currentCard.addEventListener('mouseleave', (e) => {
+    //             currentVideo.pause();
+    //         })
+    //     }
 
-        // Video cards hover play/pause
-        for (let i = 0; i < cardVideos.length; i++) {
-            let currentCard = cardVideos[i];
-            let currentVideo = currentCard.querySelector('.card-back-video')
+    //     // Video cards hover play/pause
+    //     for (let i = 0; i < cardVideos.length; i++) {
+    //         let currentCard = cardVideos[i];
+    //         let currentVideo = currentCard.querySelector('.card-back-video')
 
-            currentCard.addEventListener('mouseover', (e) => {
-                currentVideo.play();
-            })
+    //         currentCard.addEventListener('mouseover', (e) => {
+    //             currentVideo.play();
+    //         })
             
-            currentCard.addEventListener('mouseleave', (e) => {
-                currentVideo.pause();
-            })
-        }
-    }
+    //         currentCard.addEventListener('mouseleave', (e) => {
+    //             currentVideo.pause();
+    //         })
+    //     }
+    // }
 })
